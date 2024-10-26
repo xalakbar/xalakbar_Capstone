ff955582da8b2db3f4a91c155b65dd2706550609
Xana Alakbar <xalakbar@fullsail.edu>
2024-10-26 10:57:50 -0400

    Removed the email col in the users table (bookscout db) from bookscout_rs files.

--- bookscout_rs.py
+++ bookscout_rs.py
@@ -26,7 +26,6 @@ def setup_database():
     CREATE TABLE IF NOT EXISTS users (
         user_id INTEGER PRIMARY KEY,
         username TEXT NOT NULL,
-        email TEXT NOT NULL,
         password_hash TEXT NOT NULL
     )
     ''')
